9dee6faea1916327232998de4b17fd25f28b7117
Your Name <junaiyetmia470@gmil.com>
2021-05-04 23:49:05 +0600

    yhfgyu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,84 @@
+
+
+
+$(window).scroll(function(){
+  if($(window).scrollTop() > 200){
+    $(".menu").addClass("kata")
+  }else{
+    $(".menu").removeClass("kata")
+  }
+  if($(window).scrollTop() > 1000){
+    $(".backtotop").fadeIn()
+  }else{
+    $(".backtotop").fadeOut()
+  }
+
+  
+})
+
+$(".backtotop").click(function(){
+  $("html,body").animate({
+    scrollTop:0,
+  },1000)
+})
+
+
+
+
+
+
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+
+
+
+
+
+  new WOW().init();
+
+
+
+
+
+
 $('.banner-slider').slick({
   arrows: true,
   prevArrow: '<i class="fas fa-arrow-alt-circle-left prev"></i>',
